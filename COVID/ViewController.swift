ecf32daf52850c88096240cf5f782bae034f961b
s3597737 <s3597737@student.rmit.edu.au>
2020-09-05 19:02:33 +1000

    1
    1

---
+++ COVID/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  COVID
+//
+//  Created by KT on 2020/9/5.
+//  Copyright © 2020 The Riders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
